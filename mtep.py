ab261dfb1957dce9260c91967ab190851baa0430
DomAb <Dominik.Abel@tum.de>
2025-12-07 15:40:44 -0800

    refactor microphone selection process for improved usability

--- mtep.py
+++ mtep.py
@@ -1,4 +1,5 @@
 from time import sleep
+import speech_recognition as sr
 
 from jetson.context.speech import VoiceCollector, offline_stt
 
@@ -6,9 +7,16 @@ from jetson.context.speech import VoiceCollector, offline_stt
 def main():
     vc = VoiceCollector()
 
+    for i, name in enumerate(sr.Microphone.list_microphone_names()):
+        print(i, name)
+
+
 
     # add enter key so start/stop is manual for testing
-    input("Press Enter to start listening...")
+    inp = input("Enter mic number to use: ")
+
+    mic_index = int(inp)
+    vc.mic = sr.Microphone(device_index=mic_index)
 
     vc.start()
     print("Listening for 5 seconds...")
